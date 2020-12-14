fe688dee24eb07bb64c23877bb43d2fdf454453c
bami55 <1992bami@gmail.com>
2020-12-14 13:06:53 +0900

    エラー画面追加

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
@@ -27,6 +29,38 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * オリジナルデザインのエラー画面をレンダリングする
+     *
+     * @param  \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface $e
+     * @return \Illuminate\Http\Response
+     */
+    protected function renderHttpException(HttpExceptionInterface $e)
+    {
+        // デバッグモード時はLaravel標準エラー画面を表示
+        if (config('app.debug')) {
+            return parent::convertExceptionToResponse($e);
+        }
+
+        $status = $e->getStatusCode();
+        // CSRFトークンミスマッチはセッションエラーとみなす
+        if ($e instanceof TokenMismatchException) {
+            // ステータスコードを書き換える
+            $status = self::STATUS_CODE_TOKEN_MISMATCH;
+        }
+        return response()->view(
+            "errors.common", // 共通テンプレート
+            [
+                // VIEWに与える変数
+                'exception'   => $e,
+                'message'     => $e->getMessage(),
+                'status_code' => $status,
+            ],
+            $status, // レスポンス自体のステータスコード
+            $e->getHeaders()
+        );
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
@@ -37,4 +71,5 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    
 }
