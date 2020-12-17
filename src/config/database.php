2143786fccba710d865c98e234f9449488c35568
bami55 <1992bami@gmail.com>
2020-12-17 20:18:31 +0900

    change database from mysql to postgresql

--- src/config/database.php
+++ src/config/database.php
@@ -2,21 +2,24 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
+
+/*
+|--------------------------------------------------------------------------
     | PDO Fetch Style
     |--------------------------------------------------------------------------
     |
     | By default, database results will be returned as instances of the PHP
-    | stdClass object; however, you may desire to retrieve records in an
+    | stdClass object;
+however, you may desire to retrieve records in an
     | array format for simplicity. Here you can tweak the fetch style.
     |
     */
 
-    'fetch' => PDO::FETCH_CLASS,
+'fetch' => PDO::FETCH_CLASS,
 
-    /*
-    |--------------------------------------------------------------------------
+
+/*
+|--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
     |
@@ -26,10 +29,11 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+'default' => env('DB_CONNECTION', 'pgsql'),
 
-    /*
-    |--------------------------------------------------------------------------
+
+/*
+|--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
     |
@@ -44,15 +48,15 @@ return [
     |
     */
 
-    'connections' => [
+'connections' => [
 
-        'sqlite' => [
+'sqlite' => [
             'driver'   => 'sqlite',
             'database' => database_path('database.sqlite'),
             'prefix'   => '',
         ],
 
-        'mysql' => [
+'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
@@ -64,7 +68,7 @@ return [
             'strict'    => false,
         ],
 
-        'pgsql' => [
+'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -75,7 +79,7 @@ return [
             'schema'   => 'public',
         ],
 
-        'sqlsrv' => [
+'sqlsrv' => [
             'driver'   => 'sqlsrv',
             'host'     => env('DB_HOST', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -85,10 +89,11 @@ return [
             'prefix'   => '',
         ],
 
-    ],
+],
 
-    /*
-    |--------------------------------------------------------------------------
+
+/*
+|--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
     |
@@ -97,9 +102,7 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
-
     'migrations' => 'migrations',
-
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -110,18 +113,13 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
-
     'redis' => [
-
         'cluster' => false,
-
         'default' => [
             'host'     => env('REDIS_HOST', 'localhost'),
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-
     ],
-
 ];
