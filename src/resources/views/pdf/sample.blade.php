b6755766745c495269fd3328dd1912046fa1228f
bami55 <1992bami@gmail.com>
2020-12-23 08:17:15 +0900

    set page brake

--- src/resources/views/pdf/sample.blade.php
+++ src/resources/views/pdf/sample.blade.php
@@ -5,8 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <style>
+    .page-break {
+        page-break-after: always;
+    }
+    </style>
 </head>
 <body>
-  <h1>Sample</h1>
+  <h1>Page 1</h1>
+  <div class="page-break"></div>
+  <h1>Page 2</h1>
 </body>
 </html>
